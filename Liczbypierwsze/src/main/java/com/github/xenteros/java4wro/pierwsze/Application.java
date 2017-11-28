4491a772e370bdfc869aef195384cbb0dc3118c3
xenteros <adam@gurgul.com.pl>
2017-11-28 18:46:10 +0100

    Add LeapYearService

---
+++ Liczbypierwsze/src/main/java/com/github/xenteros/java4wro/pierwsze/Application.java
@@ -0,0 +1,11 @@
+package com.github.xenteros.java4wro.pierwsze;
+
+import com.github.xenteros.java4wro.pierwsze.service.LeapYearService;
+
+public class Application {
+
+    public static void main(String[] args) {
+        LeapYearService leapYearService = new LeapYearService();
+        System.out.println(leapYearService.isLeapYear(1900));
+    }
+}
---
+++ Liczbypierwsze/src/main/java/com/github/xenteros/java4wro/pierwsze/service/LeapYearService.java
@@ -0,0 +1,22 @@
+package com.github.xenteros.java4wro.pierwsze.service;
+
+public class LeapYearService {
+
+    /*
+        zwróć prawdę, gdy:
+            jest podzielny przez 400
+                lub
+            jest podzielny przez 4 i niepodzielny przez 100
+
+        wpp zwróć fałsz
+     */
+    public boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
